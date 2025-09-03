11588582519c0a139b75468271f5ea6d4867e73b
Julien <maire94@gmail.com>
2025-09-03 12:22:28 +0200

    init

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 import express from 'express';
-import mail from './routerTest.js';
+import createAccount from './createAccount.js'; //julien
+import contact from './contact.js';	//Jerem
+import password from './password.js'; //Paulé
+import welcome from './welcome.js';	//Nihel
 
 const port = 3000;
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use('/createAccount', createAccount);
+app.use('/contact', contact);
+app.use('/password', password);
+app.use('/welcome', welcome);
+
 app.get('/', (req, res) => {
 	res.send('Coucou');
 });
-
-app.use('/mail', mail);
 
 app.listen(port);
--- routerTest.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { sendMail } from './controllerMail.js';
-
-const router = express.Router();
-
-router.get('/', sendMail);
-
-export default router;
