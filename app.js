182844cb0092fc1c696171e664766e00c3a0e391
Julien <maire94@gmail.com>
2025-09-03 12:27:02 +0200

    init 2

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 import express from 'express';
-import createAccount from './createAccount.js'; //julien
-import contact from './contact.js';	//Jerem
-import password from './password.js'; //Paulé
-import welcome from './welcome.js';	//Nihel
+import createAccount from './accountRouter.js'; //julien
+import contact from './contactRouter.js';	//Jerem
+import password from './passwordRouter.js'; //Paulé
+import welcome from './welcomeRouter.js';	//Nihel
 
 const port = 3000;
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use('/createAccount', createAccount);
+app.use('/account', account);
 app.use('/contact', contact);
 app.use('/password', password);
 app.use('/welcome', welcome);
